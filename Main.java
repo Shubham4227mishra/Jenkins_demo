d35260ac81f70cb215451ded859d8da2fd40f1c7
Shubham4227mishra <167312452+Shubham4227mishra@users.noreply.github.com>
2024-04-23 20:05:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main 
+{
+public static void main(String[] sam)
+{
+String name;
+int age;
+System.out.println("enter your name : ");
+System.out.println("enter your age : ");
+Scanner a=new Scanner(System.in);
+name=a.nextLine();
+age=a.nextInt();
+System.out.println(" my name is " +  name  + " and age is " + age);
+
+}
+}
+
